d8355b3f2395c4f3221d525ada6590cfb5f9d423
FroVolod <frol_off@meta.ua>
2019-09-21 11:01:27 +0300

    added courses

---
+++ backend/app/constants.py
@@ -0,0 +1,49 @@
+TOURNAMENT_PARTICIPANTS = ['single', 'group', ]
+
+COURSES_DICT = [
+    {
+        'course_title': 'Основы программирования',
+        'contests': [
+            {
+                'contest_title': 'Привет Мир!',
+                'duration': '3600',
+                'problems': ['1000', '1001', '1002', '1003']
+            },
+            {
+                'contest_title': 'Вспомним, товарищи!',
+                'duration': '3600',
+                'problems': ['1003', '1004', '1004', '1006']
+            }
+        ]
+    },
+    {
+        'course_title': 'Базовые алгоритмы',
+        'contests': [
+            {
+                'contest_title': 'Сортировка и поиск',
+                'duration': '3600',
+                'problems': ['1007', '1008', '1009']
+            },
+            {
+                'contest_title': 'Сортировка и поиск',
+                'duration': '3600',
+                'problems': ['1010', '1011', '1012']
+            }
+        ]
+    },
+    {
+        'course_title': 'Тренування1',
+        'contests': [
+            {
+                'contest_title': 'Лабораторная работа №1',
+                'duration': '3600',
+                'problems': ['1007', '1008', '1009']
+            },
+            {
+                'contest_title': 'Лабораторная работа №2',
+                'duration': '3600',
+                'problems': ['1010', '1011', '1012']
+            }
+        ]
+    }
+]
