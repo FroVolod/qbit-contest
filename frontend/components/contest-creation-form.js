2b91dbfa21a199dd8f327720cd4c281b612898f0
FroVolod <frol_off@meta.ua>
2019-11-08 12:52:48 +0200

    checked  "info", "allowLanguages"

--- frontend/components/contest-creation-form.js
+++ frontend/components/contest-creation-form.js
@@ -1,4 +1,4 @@
-import { Col, Button, Form, FormGroup, Label, Input, InputGroup, InputGroupAddon, InputGroupText, Spinner } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input, Text, InputGroup, InputGroupAddon, InputGroupText, Spinner } from 'reactstrap';
 
 import 'moment/locale/uk' // or 'rc-datepicker/node_modules/moment/locale/fr.js' if you don't have it in your node_modules folder
 import { DatePickerInput } from 'rc-datepicker';
@@ -13,27 +13,41 @@ import { now } from 'moment';
 
 export default class extends React.Component {
     state = {
-        author: null,
+        author: 1007,
         contestType: 'otbor',
         courseTitle: null,
         contestTitle: null,
         problems: null,
         allowLanguages: null,
         startTime: null,
-        contestParticipants: null,
+        contestParticipantsGroups: [],
         options: 4,
         data: null,
-        info: null,
+        info: 'информация',
         date: new Date(),
         courses: null,
         contestsDict: null,
         excludedProblems: [],
-        excludedAllowLanguages: [],
+        programmingLanguagesEnabled: {},
+        participantsGroups: [],
+        showParticipantsGroupsSpinner: true,
     }
 
     componentDidMount() {
         console.log('DIDMount open')
         if (!this.props.state.session) return
+        this.getParticipantsGroups(this.state.author)
+            .then((participantsGroups) => {
+                this.setState(
+                    {
+                        participantsGroups,
+                        showParticipantsGroupsSpinner: false
+                    }
+                )
+            })
+            .catch((e) => (
+                console.log('componentDidMount: setting the contestParticipans error -- ', e)
+            ))
         this.getTitlesCourses()
             .then((courses) => console.log('DIDMount getTitlesCourses response: ', courses))
             .catch((e) => console.log('DIDMount getTitlesCourses error:', e)) 
@@ -59,12 +73,31 @@ export default class extends React.Component {
             .catch((e) => console.log('DidUpdate getTitlesCourses error:', e))
         console.log('DidUpdate: Loading the titles of the courses ...')
         console.log('DidUpdate exit', this.state.courses, 'session: ', this.props.state.language)
+        if (this.state.contestParticipansGroup && this.props===nextProps) return
+        this.getParticipantsGroups(this.state.author)
+            .then((participantsGroups) => {
+                console.log('DidUpdate getParticipantsGroups response: ', participantsGroups)
+                if (participantsGroups === this.state.participantsGroups) return
+                console.log('componentDidUpdate: setting state.conestParticipantsGroups')
+                this.setState(
+                    {
+                        participantsGroups,
+                        showParticipantsGroupsSpinner: false
+                    }
+                )
+            })
+            .catch((e) => (
+                console.log('componentDidUpdate: setting the contestParticipans error -- ', e)
+            ))
         if (this.state.allowLanguages) return
         this.getAllowLanguage()
             .then((allowLanguages) => {
                 console.log('DidUpdate getAllowLanguage response: ', allowLanguages)
-                if (this.state.allowLanguages === allowLanguages) return
-                this.setState({allowLanguages})
+                const programmingLanguagesEnabled = {};
+                for (const lang of allowLanguages) {
+                    programmingLanguagesEnabled[lang] = true;
+                }
+                this.setState({allowLanguages, programmingLanguagesEnabled})
             })
             .catch((e) => console.log('DidUpdate getAllowLanguage error:', e))
     }
@@ -73,7 +106,7 @@ export default class extends React.Component {
         console.log('On click "Create contest"')
         let problems = this.state.problems.filter(x => !this.state.excludedProblems.includes(x));
         console.log('createContest problems: ', problems)
-        let allowLanguages = this.state.allowLanguages.filter(x => !this.state.excludedAllowLanguages.includes(x));
+        let allowLanguages = this.state.allowLanguages.filter(lang => this.state.programmingLanguagesEnabled[lang])
         console.log('createContest allowLanguages:', allowLanguages)
         const res = await this.props.state.session.call('com.demo.create-contest', ['create-contest',])
         console.log('res: ', res)
@@ -90,6 +123,52 @@ export default class extends React.Component {
         this.getContestsDict(e.target.value)
     }
 
+    onAuthorChange = (e) => {
+        console.log('Setting the ContestParticipantsGroups with: ', e.target.value)
+        this.setState({showParticipantsGroupsSpinner: true})
+        this.getParticipantsGroups(e.target.value)
+            .then((participantsGroups) => (
+                this.setState(
+                    {
+                        participantsGroups,
+                        showParticipantsGroupsSpinner: false
+                    }
+                )
+            ))
+            .catch((e) => (
+                console.log('onAuthorChange: setting the contestParticipans error -- ', e)
+            ))
+    }
+
+    setAuthor = (author) => {
+        this.setState({author})
+    }
+
+    getParticipantsGroups = async (author) => {
+        console.log('getParticipantsGroups value: ', author)
+        const participantsGroups = await this.props.state.session.call('com.demo.get-participants-groups', [author,])
+        console.log('participantsGroups', participantsGroups)
+        if (author !== this.state.author) {
+            this.setAuthor(author)
+        }
+        return participantsGroups
+    }
+
+    setContestParticipantsGroups = (e) => {
+        console.log('Setting the setContestParticipantsGroups with: ', e.target.checked, e.target.id)
+        let contestParticipantsGroups = this.state.contestParticipantsGroups
+        if (e.target.checked) {
+            contestParticipantsGroups.push(e.target.id)
+        }
+        else {
+            let value = contestParticipantsGroups.indexOf(e.target.id)
+            if (value !== -1) {
+                contestParticipantsGroups.splice(value, 1)
+            }
+        }
+        this.setState({contestParticipantsGroups})
+    }
+
     getContestsDict = async (courseID) => {
         console.log('Getting the titles of the contests with courseID: ', courseID)
         const contestsDict = await this.props.state.session.call('com.demo.get-contests-dictionaries', [courseID, this.props.state.language])
@@ -101,17 +180,20 @@ export default class extends React.Component {
             problems: contestsDict[0]['problems'],
             contestTitle: contestsDict[0]['contest_title'],
             courseTitle: contestsDict[0]['course_title'],
+            info: contestsDict[0]['info'],
         })
     }
 
-    getTitlesProblems = (contest) => {
+    getTitlesProblemsAndInfo = (contest) => {
         console.log('Getting the problems of the contest: ', contest)
         console.log('*** contestsDict: ', this.state.contestsDict)
-        const problems = this.state.contestsDict.find(item => item.contest_title == contest).problems
-        console.log('*** problems', problems)
+        const item = this.state.contestsDict.find(item => item.contest_title == contest)
+        console.log('*** problems', item.problems)
+        console.log('*** info ', item.info)
         this.setState({
-            problems,
+            problems: item.problems,
             contestTitle: contest,
+            info: item.info
         })
     }
 
@@ -131,25 +213,31 @@ export default class extends React.Component {
         this.setState({excludedProblems})
     }
 
+    onToggleAllProgrammingLanguages = (e) => {
+        const programmingLanguagesEnabled = {};
+        for (const lang of this.state.allowLanguages) {
+            programmingLanguagesEnabled[lang] = e.target.checked;
+        }
+        this.setState({programmingLanguagesEnabled})
+    }
+
     onChangeAllowLanguagesList = (e) => {
-        console.log('onChangeAllowLanguagesList: ', e.target.id, e.target.checked)
-        let excludedAllowLanguages = this.state.excludedAllowLanguages;
-        if (!e.target.checked) {
-            excludedAllowLanguages.push(e.target.id)
-        }
-        else {
-            let value = excludedAllowLanguages.indexOf(e.target.id);
-            if (value !== -1) {
-                excludedAllowLanguages.splice(value, 1)
+        console.log('onChangeAllowLanguagesList: ', e.target.id, e.target.checked, this.state.programmingLanguagesEnabled)
+        const lang = e.target.name;
+        const checked = e.target.checked;
+        this.setState((state) => {
+            return {
+                programmingLanguagesEnabled: {
+                    ...state.programmingLanguagesEnabled,
+                    [lang]: checked,
+                }
             }
-        }
-        console.log('onChangeAllowLanguagesList excludedAllowLanguages: ', excludedAllowLanguages)
-        this.setState({excludedAllowLanguages})
+        });
     }
 
     setContest = (e) => {
         console.log('Setting contest ...', e.target.value)
-        this.getTitlesProblems(e.target.value)
+        this.getTitlesProblemsAndInfo(e.target.value)
     }
 
     getTitlesCourses = async () => {
@@ -172,6 +260,11 @@ export default class extends React.Component {
         this.setState({contestType: e.target.value}) 
     }
 
+    setInfo = (e) => {
+        console.log('Setting info with: ', e.target.value)
+        this.setState({info: e.target.value})
+    }
+
     render() {
         if (!this.props.state.session) return null
         console.log('** render() ** contest creation form (session):', this.props.state.session)
@@ -183,13 +276,32 @@ export default class extends React.Component {
                 <FormGroup row>
                     <Label sm={2} for="author_id">Автор</Label>
                     <Col sm={6}>
-                        <Input type="textarea" name="text" id="author_id" placeholder="автоподставка" />
+                        <Input type="textarea" name="text" id="author_id" placeholder={this.state.author} onBlur={this.onAuthorChange}>
+                            {this.state.author}
+                        </Input>
                     </Col>            
                 </FormGroup>
+                {!this.state.participantsGroups ? null : 
+                    <FormGroup row>
+                        <Label sm={2} for="participants">Участники турнира</Label>
+                        <Col sm={6}>
+                            {this.state.participantsGroups.map((group) => (
+                                <InputGroup key={group}>
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>
+                                        <Input id={group} onChange={this.setContestParticipantsGroups} addon type="checkbox"  />
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                    <Input placeholder={group}></Input>
+                                </InputGroup>
+                            ))}
+                        </Col>
+                    </FormGroup>
+                }
                 <FormGroup row>
                     <Label sm={2} for="contest_type">Тип турнира</Label>
                     <Col sm={6}>
-                        <Input onChange={this.setContestType} type="select" name="select" id="contest_type">
+                        <Input onChange={this.setContestType} type="select" name="selectContestType" id="contest_type">
                             <option value="otbor" >Практическая работа</option>
                             <option value="labs" >Лабораторная работа</option>
                             <option value="olympic" >Олімпіада</option>
@@ -205,33 +317,37 @@ export default class extends React.Component {
                 <FormGroup row>
                     <Label sm={2} for="title">Название курса</Label>
                     <Col sm={6}>
-                        <Input  onChange={this.onCourseChange} type="select" name="select" id="contest_title">                        
-                            {this.state.courses === null ? null : this.state.courses.map((course) => (
-                                <option key={course.course_id} value={course.course_id}>{course.course_title}</option>
-                            ))}
+                        <Input  onChange={this.onCourseChange} type="select" name="selectCours" id="contest_title">                        
+                            {!this.state.courses
+                                ? null
+                                : this.state.courses.map((course) => (
+                                    <option key={course.course_id} value={course.course_id}>{course.course_title}</option>
+                                ))
+                            }
                         </Input>
                     </Col>
                     {this.state.courses ? null : <Col sm={4}><Spinner color="warning" /></Col>}
                     
                 </FormGroup>
                 
-                {this.state.courses === null ? null :
+                {!this.state.courses ? null :
                     <FormGroup row>
                         <Label sm={2} for="contest_title">Название турнира</Label>
                         <Col sm={6}>
                             <Input onChange={this.setContest} type="select" name="select" id="contest_title">
-                                {this.state.contestsDict === null ? null : this.state.contestsDict.map((contest) => (
-                                    <option key={contest.contest_title}>{contest.contest_title}</option>
-                                ))}
+                                {!this.state.contestsDict
+                                    ? null
+                                    : this.state.contestsDict.map((contest) => (
+                                        <option key={contest.contest_title}>{contest.contest_title}</option>
+                                    ))
+                                }
                             </Input>
                         </Col>
                     </FormGroup>
                 }
-                {this.state.problems === null ? null :
+                {!this.state.problems ? null :
                     <FormGroup row>
-                        
                         <Label sm={2} for="problems">Задачи турнира</Label>
-                        
                         <Col sm={6}>
                             {this.state.problems.map((problem) => (
                                 <InputGroup key={problem}>
@@ -249,16 +365,37 @@ export default class extends React.Component {
                 <FormGroup row>
                     <Label sm={2} for="allow_languages">Разрешенные языки программирования</Label>
                     <Col sm={6}>
-                        {this.state.allowLanguages === null ? null : this.state.allowLanguages.map((lang) => (
-                            <InputGroup key={lang}>
-                                <InputGroupAddon addonType="prepend">
-                                <InputGroupText>
-                                    <Input id={lang} onChange={this.onChangeAllowLanguagesList} addon type="checkbox" defaultChecked />
-                                </InputGroupText>
-                                </InputGroupAddon>
-                                <Input placeholder={lang}></Input>
-                            </InputGroup>
-                        ))}
+                        <InputGroup key={lang}>
+                            <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                                <Input onChange={this.onToggleAllProgrammingLanguages} addon type="checkbox" checked={Object.values(this.state.programmingLanguagesEnabled).every(x => x)} />
+                            </InputGroupText>
+                            <InputGroupText>
+                                Выбрать все
+                            </InputGroupText>
+                            </InputGroupAddon>
+                        </InputGroup>
+                        {!this.state.allowLanguages
+                            ? null
+                            : this.state.allowLanguages.map((lang) => (
+                                <InputGroup key={lang}>
+                                    <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>
+                                        <Input name={lang} onChange={this.onChangeAllowLanguagesList} addon type="checkbox" checked={this.state.programmingLanguagesEnabled[lang]} />
+                                    </InputGroupText>
+                                    <InputGroupText>
+                                        {lang}
+                                    </InputGroupText>
+                                    </InputGroupAddon>
+                                </InputGroup>
+                            ))
+                        }
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label sm={2} for="info">info</Label>
+                    <Col sm={6}>
+                        <Input type="textarea" name="text" id="info"  value={this.state.info} onChange={this.setInfo} />
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -281,25 +418,11 @@ export default class extends React.Component {
                         // {...anyReactInputProps}
                         />
                     </Col>
-                    
-
-                </FormGroup>
-                <FormGroup row>
-                    <Label sm={2} for="participants">Участники турнира</Label>
-                    <Col sm={6}>
-                        <Input type="textarea" name="text" id="participants" />
-                    </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label sm={2} for="data">data</Label>
                     <Col sm={6}>
                         <Input type="textarea" name="text" id="data" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Label sm={2} for="info">info</Label>
-                    <Col sm={6}>
-                        <Input type="textarea" name="text" id="info" />
                     </Col>
                 </FormGroup>
                 <FormGroup check row>
